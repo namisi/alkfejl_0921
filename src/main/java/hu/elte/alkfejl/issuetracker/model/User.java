1f1bff63db334faff6e119170777c1e10cbc60b5
namtaai <naszradi@gmail.com>
2017-09-28 15:08:16 +0200

    java file moved to the correct package create data.sql

--- src/main/java/hu/elte/alkfejl/issuetracker/model/User.java
+++ src/main/java/hu/elte/alkfejl/issuetracker/model/User.java
@@ -5,9 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "USERS")
